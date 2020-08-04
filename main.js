727fa61c170561f986ae1765d96608ccfcd8793e
MrMoshkovitz <gal@gmail.com>
2020-08-04 09:32:29 +0300

    start

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-const searchPokemon = async (pokemonId = 3) => {
+const searchResults = document.getElementById("searchResults")
+const search = document.getElementById("search")
+const searchButon = document.getElementById("searchButon")
+const pokemonClassName = document.getElementsByClassName("pokemonName")
+const pokemonClassId = document.getElementsByClassName("pokemonId")
+const pokemonClassHeight = document.getElementsByClassName("pokemonHeight")
+const pokemonClassWeight = document.getElementsByClassName("pokemonWeight")
+const pokemonClassImage = document.getElementsByClassName('pokemonImage')
+
+var pokemonsDeck = [];
+
+const searchPokemon = async () => {
+  const pokemonId = search.value;
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data)
-};
-searchPokemon();
 
+  const pokemonObject = {
+    name: data.name,
+    id: pokemonId,
+    height: data.height,
+    weight: data.weight,
+    backIMG: data.sprites.back_female,
+    frontIMG: data.sprites.front_female
+  };
+  console.log(pokemonObject);
+};
+
+
+
+
+const searchValidation= async () => {
+  const input = search.value;
+  const { data } = await axios.get("http://pokeapi.co/api/v2/pokemon/");
+  const count = data.count;
+  error = document.getElementById("error");
+  console.log(`Search Value: ${input}`);
+  console.log(`Count Of Pokemons: ${count}`);
+  if (input != parseInt(input, 10)){
+    error.style.opacity = "1";
+    error.style.height = "50";
+    error.style.color = "red";
+    error.innerText = "Pokemon ID must be of type Number!";
+  } else {
+    searchPokemon();
+  }
+};
