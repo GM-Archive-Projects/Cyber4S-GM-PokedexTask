ba73e6036202d3e3b0fe2d67d19d29e95d0c6daf
MrMoshkovitz <gal@gmail.com>
2020-08-04 11:11:58 +0300

    a

---
+++ Test/main.js
@@ -0,0 +1,39 @@
+// defaults
+axios.defaults.baseURL = "http://pokeapi.co/api/v2/pokemon/";
+axios.defaults.validateStatus = status => status >= 200 && status < 300;
+
+function getPokemon(pokemonId) {
+  axios.get(`/${pokemonId}`).then(function(response){
+    console.log(response.data);
+    })
+    .catch(function(error){
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    })}
+    getPokemon(999)
+
+/* 
+function getResource(resource) {
+    return axios.get("/" + resource).then(r => r.data);
+}
+
+function postResource(resource, resourceData) {
+    return axios.post("/" + resource, resourceData).then(r => r.data);
+}
+
+(async function main() {
+  const resources = await getResources();
+  console.log(resources);
+  
+  if (resources.length > 0) {
+    const resource = await getResource(resources[0]);
+    console.log(resource);
+  }
+
+  const testResource = { x: 5, y: 7 };
+  const response = await postResource("test", testResource);
+  console.log(response);
+})();
+ 
+ */
--- main.js
+++ main.js
@@ -6,24 +6,28 @@ const pokemonClassId = document.getElementsByClassName("pokemonId")
 const pokemonClassHeight = document.getElementsByClassName("pokemonHeight")
 const pokemonClassWeight = document.getElementsByClassName("pokemonWeight")
 const pokemonClassImage = document.getElementsByClassName('pokemonImage')
+axios.defaults.baseURL = "http://pokeapi.co/api/v2/pokemon/";
 
 var pokemonsDeck = [];
 
 const searchPokemon = async () => {
   const pokemonId = search.value;
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
-
-  const pokemonObject = {
-    name: data.name,
-    id: pokemonId,
-    height: data.height,
-    weight: data.weight,
-    backIMG: data.sprites.back_female,
-    frontIMG: data.sprites.front_female
+  try {
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    console.log(data)
+    
+    const pokemonObject = {
+      name: data.name,
+      id: pokemonId,
+      height: data.height,
+      weight: data.weight,
+      backIMG: data.sprites.back_female,
+      frontIMG: data.sprites.front_female
   };
-  console.log(pokemonObject);
-};
+    console.log(pokemonObject);
+} catch (error) {
+  console.log(error)
+}};
 
 
 
@@ -39,6 +43,7 @@ const searchValidation= async () => {
     error.style.opacity = "1";
     error.style.height = "50";
     error.style.color = "red";
+    error.style.display = "block";
     error.innerText = "Pokemon ID must be of type Number!";
   } else {
     searchPokemon();
