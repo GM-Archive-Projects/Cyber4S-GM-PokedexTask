6dff31e47264cba795c1e47ea6e84baffbc8d9b3
MrMoshkovitz <gal@gmail.com>
2020-08-12 12:54:22 +0300

    Adding Main js and deleting index.html button
    Adding Global ENv and Axios Defaults

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const searchPokemon = async (pokemonId = 3) => {
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
-};
-searchPokemon();
+// Global Scope Dom Environment Variables 
+const search = document.getElementById("search");
+const searchButton = document.getElementById("searchButton");
+const searchResults = document.getElementById("searchResults");
+const poke_container = document.getElementById("poke_container");
+const errorElement = document.getElementById("error")
+const pokesListElem = document.getElementById("pokeList")
+
+
+
+//Defining Axios Defaults
+import axios from 'axios';
+axios.defaults.baseURL = "http://pokeapi.co/api/v2/";
+axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
 
