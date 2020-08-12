a78d292dc1702ef7275b7dc197a7fd60b7e46e38
MrMoshkovitz <gal@gmail.com>
2020-08-12 12:55:12 +0300

    Adding Color Type Mapping

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ import axios from 'axios';
 axios.defaults.baseURL = "http://pokeapi.co/api/v2/";
 axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
 
+// Mapping Pokemon Coloes By Types
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
